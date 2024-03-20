47ffb153f6b993aa4ff519a87c13f94e6a754604
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-03-20 23:21:58 +1100

    Added title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,10 @@
 </script>
 
 
+<head>
+    <title>MCT32.xyz</title>
+</head>
+
 <p class="text-white text-3xl p-10 font-semibold max-w-[40rem]">
     Hello! I'm <Popout href="https://github.com/MCT32">MCT32</Popout>, a developer who can't seem to finish his projects...
     <br>
