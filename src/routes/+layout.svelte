16e84936896d2185dd232b51ba72584469d13087
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-03-20 16:08:29 +1100

    Made heading fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
     import "../app.css";
 </script>
-  
-<slot />
+
+
+<div class="shadow-xl p-2.5 flex items-center gap-x-10 fixed w-full bg-zinc-800">
+    <h1 class="text-white font-extrabold text-5xl w-fit"><a href="https://mct32.xyz">MCT32.xyz</a></h1>
+
+    <a href="/" class="w-fit text-white font-semibold">Home</a>
+    <a href="/projects" class="w-fit text-white font-semibold">Projects</a>
+    <a href="/about" class="w-fit text-white font-semibold">About</a>
+</div>
+
+<div class="pt-20">
+    <slot />
+</div>
