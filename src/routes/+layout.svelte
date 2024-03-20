bed0bccaab519ac0315c0e00871083da3f98b2f2
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-03-20 23:17:18 +1100

    Refactoring

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,16 @@
 </script>
 
 
-<div class="shadow-xl p-2.5 flex items-center gap-x-10 fixed w-full bg-zinc-800">
+<div class="relative min-h-screen">
+    <nav class="shadow-xl p-2.5 flex items-center gap-x-10 sticky top-0 w-full bg-zinc-800">
     <h1 class="text-white font-extrabold text-5xl w-fit"><a href="https://mct32.xyz">MCT32.xyz</a></h1>
 
     <a href="/" class="w-fit text-white font-semibold">Home</a>
     <a href="/projects" class="w-fit text-white font-semibold">Projects</a>
     <a href="/about" class="w-fit text-white font-semibold">About</a>
-</div>
+    </nav>
 
-<div class="pt-20">
+    <div>
     <slot />
+    </div>
 </div>
