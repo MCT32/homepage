ebd1539834df06b7e38af0a65e844782ebd5b810
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-03-21 12:05:29 +1100

    hides navbar on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,9 +15,11 @@
     <nav class="shadow-xl p-2.5 flex items-center gap-x-10 sticky top-0 w-full bg-zinc-800">
         <h1 class="text-white font-extrabold text-5xl w-fit"><a href="https://mct32.xyz">MCT32.xyz</a></h1>
 
-        <a href="/" class="w-fit text-white font-semibold">Home</a>
-        <a href="/projects" class="w-fit text-white font-semibold">Projects</a>
-        <a href="/about" class="w-fit text-white font-semibold">About</a>
+        <div class="md:flex gap-x-10 hidden">
+            <a href="/" class="w-fit text-white font-semibold">Home</a>
+            <a href="/projects" class="w-fit text-white font-semibold">Projects</a>
+            <a href="/about" class="w-fit text-white font-semibold">About</a>
+        </div>
     </nav>
 
     <div>
