4495a2aa5aa4e4490d2f0b7f11a4beff33222f1d
MCT32 <32090502+MCT32@users.noreply.github.com>
2024-03-26 18:05:01 +1100

    About page done

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,13 @@
+<img src="/pfpv3.png" alt="Pfp" class="rounded-full mx-auto my-10">
+
+<div class="grid grid-cols-3 max-w-[80rem] mx-auto">
+    <a href="mailto:MCT32@proton.me" class="text-white max-w-[60rem] mx-auto text-3xl inline-grid">Email</a>
+    <a href="https://github.com/MCT32" class="text-white max-w-[60rem] mx-auto text-3xl inline-grid">Github</a>
+    <a href="https://discordapp.com/users/456685402393411594" class="text-white max-w-[60rem] mx-auto text-3xl inline-grid">Discord</a>
+</div>
+
+<style lang="postcss">
+    :global(body) {
+        background-color: theme('colors.zinc.800');
+    }
+</style>
