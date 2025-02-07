afad5df26f2e0e52b2fff1e54b61950e81b2190c
MCT32 <ivanhen99@gmail.com>
2025-02-08 01:21:16 +1100

    Made blogs page feel more animated

--- src/lib/components/blogCard.svelte
+++ src/lib/components/blogCard.svelte
@@ -5,11 +5,11 @@
     export let link: string;
     export let title: string;
     export let description: string;
-    export let image: string = "/";
+    export let image: string = "";
     export let tags: string[] = [];
 </script>
 
-<div class="w-fill h-52 border rounded-md border-zinc-500 p-3" style="background-image: url('{image}');" on:click={() => goto(link)}>
+<div class="relative w-fill h-52 border rounded-md border-zinc-500 p-3 cursor-pointer transition hover:scale-105" style="background-image: url('{image}');" on:click={() => goto(link)}>
     <h2 class="text-white font-bold text-xl">{title}</h2>
 
     <div class="flex gap-1">
@@ -19,4 +19,6 @@
     </div>
 
     <p>{description}</p>
+
+    <div class="absolute top-0 left-0 w-full h-full hover:bg-zinc-500 hover:bg-opacity-10 active:bg-opacity-25" />
 </div>
