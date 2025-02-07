640b60a7a0adc12fca4a10cca368b6375f597d0a
MCT32 <ivanhen99@gmail.com>
2025-02-08 00:47:25 +1100

    blog list concept

---
+++ src/lib/components/blogCard.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import BlogTag from "./blogTag.svelte";
+
+    export let link: string;
+    export let title: string;
+    export let description: string;
+    export let image: string = "/";
+    export let tags: string[] = [];
+</script>
+
+<div class="w-fill h-52 border rounded-md border-zinc-500 p-3" style="background-image: url('{image}');" on:click={() => goto(link)}>
+    <h2 class="text-white font-bold text-xl">{title}</h2>
+
+    <div class="flex gap-1">
+        {#each tags as tag}
+            <BlogTag {tag} />
+        {/each}
+    </div>
+
+    <p>{description}</p>
+</div>
---
+++ src/lib/components/blogTag.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import PhHashBold from 'virtual:icons/ph/hash-bold';
+
+    export let tag: string;
+</script>
+
+<div class="bg-zinc-600 rounded-full px-2 text-zinc-400">
+    <PhHashBold class="inline-block" />
+    {tag}
+</div>
